a4640827dcbfbfd1666441ef74640928f48a1d6d
Rickos312 <190382928+Rickos312@users.noreply.github.com>
2025-07-17 20:12:09 +0200

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://mxegclbmbtuaeohcohhn.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Variables d\'environnement Supabase manquantes');
