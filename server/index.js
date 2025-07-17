b1d13b6fb7296037b4656f63659b2ae7698b4601
Rickos312 <190382928+Rickos312@users.noreply.github.com>
2025-07-17 13:29:35 +0200

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3006;
 
 // Middleware
 app.use(cors({
